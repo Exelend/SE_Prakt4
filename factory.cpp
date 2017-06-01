ce57fff92db33c462c68e9329c7caf78b5aaa134
Hauke <hauke.goldhammer@haw-hamburg.de>
2017-06-01 09:41:03 +0200

    Factory

--- factory.cpp
+++ factory.cpp
@@ -16,7 +16,7 @@ FSM* FSMFactory::createFSM(){
        // Create Objects
     FestoProcessImage* processImage = new FestoMiniLab1008ProcessImage();
     FestoProcessAccess* processAccess = new FestoProcessAccess(processImage);
-    Plugin* plugin = new Plugin(processAccess);
+    Plugin* plugin = new HeightLogic(processAccess);
     FSM* fsm = new FSM(processAccess, plugin);
     return fsm;
 }
